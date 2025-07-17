986c781901a530f8225792799852840ae874d1be
Erick Gonzalez Cruz <176557431+DatOneGuy77@users.noreply.github.com>
2025-07-17 00:12:29 -0700

    learn about arrays

---
+++ Practice.py
@@ -0,0 +1,29 @@
+"""
+Erick Gonzalez Cruz
+File: This is to practice my knowledge of arrays!!!
+"""
+list_ex = [1, 2, 4, 8]
+
+list_ex.insert(2, 3)
+
+print(list_ex)
+
+print("-" * 100)
+
+list_ex.pop(2)
+
+print(list_ex)
+
+print("-" * 100)
+
+list_ex[0] = 30 #replair 1 with 30 
+
+print(list_ex)
+#Reverse an array 
+# list_1 = [1, 2 , 3, 4]
+
+# start = 0 
+# end = list_1[3] - 1
+
+# while start < end:
+#     start[0]
