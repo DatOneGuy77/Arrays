ba3aa544a20bf153a76fd9322550554c5cbb80b1
Erick Gonzalez Cruz <176557431+DatOneGuy77@users.noreply.github.com>
2025-07-19 23:55:53 -0700

    idk

--- nax_and_min.py
+++ nax_and_min.py
@@ -4,10 +4,28 @@ Date: 7/18/2025
 File: Finding the Max and Min value in an array!
 """
 
-# Find the max/min value of an array
+# Find the max value of an array
 list_2 = [5, 20, 45, 11, 94]
 
-max_val = list_2[0]
+max_val = max(list_2)
 
-for i in list_2:
-    start 
+print(max_val)
+print("-" * 100)
+
+# Find the min value of an array
+list_3 = [8, 10, 492, 321, 45, 78, 97]
+
+min_val = min(list_3)
+print(min_val)
+
+#Now lets create a function with this: 
+def max_value():
+    list_3 
+    return max_val
+
+
+# start = list_2[0] #start now contains index 0 which has the value of 5
+# max_val = .ma
+
+# while 
+     
