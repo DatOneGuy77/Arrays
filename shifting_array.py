d192f2bc73e5775c3d9d13a37665fe9c215dfa69
Erick Gonzalez Cruz <176557431+DatOneGuy77@users.noreply.github.com>
2025-07-23 18:24:22 -0700

    better shifting left by 1

--- shifting_array.py
+++ shifting_array.py
@@ -15,7 +15,6 @@ def shift_left_by_1(any_list):
 
     for i in range(1, len(any_list)):
         any_list[i - 1] = any_list[i]
-
     any_list[-1] = start
 
     return any_list
@@ -24,13 +23,29 @@ print(shift_left_by_1(list_1))
 
 #This time we shift by the right
 list_2 = [20, 50, 80 ,90, 100]
+# # Original: [20, 50, 80 ,90, 100]
+# # Result:   [100, 20, 50, 80, 90]
+
+# print(list_2)
+
+# def shift_right_by_1(any_list):
+#     shifted_list = [any_list[-1]] + any_list[:-1]
+#     return shifted_list
+
+# print(shift_right_by_1(list_2))
+
+print("-" * 100)
+
 # Original: [20, 50, 80 ,90, 100]
-# Result:   [100, 20, 50, 80, 90]
+# Result:   [50, 80, 90, 100, 20]
 
-print(list_2)
+#This is jsut a faster and less confusing version of my previous shifting left function:
+def shifting_left(any_list):
+    first_element = any_list[0] #get the very first index from the list
+    any_list[-1] = first_element #The last index of this list will now be the OG first index
 
-def shift_right_by_1(any_list):
-    shifted_list = [any_list[-1]] + any_list[:-1]
+    shifted_list = any_list [1:] + [first_element]
+
     return shifted_list
 
-print(shift_right_by_1(list_2))
+print(shifting_left(list_2))
