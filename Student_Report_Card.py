06a71e7f5d4696856cec3eabd367edf56740348b
Erick Gonzalez Cruz <176557431+DatOneGuy77@users.noreply.github.com>
2025-07-25 23:19:43 -0700

    Comments and working prototype

--- Student_Report_Card.py
+++ Student_Report_Card.py
@@ -3,31 +3,48 @@ Erick Gonzalez Cruz
 07/23/2025
 File: This is a reprt card system using Object oriented proggraming in the data-structure array! 
 """
+import random 
 
 class Student:
     def __init__(self, student_name):
         self.student_name = student_name
 
-    def student_names(self):
+    def student_names(self): #List of all student names
         name_list = ["Erick", "Diego", "Ivan", "Omar", "Isaac"]
         return name_list
 
     def student_letter_grade(self):
-        pass 
+        grade_list = ["A", "B", "C", "D", "F"] #List containing all letter grades
 
-class Grade:
-    def __init__(self):
-        pass 
-        
-    def student_percentage_grade(self):
-        pass
-    pass 
+        percentage = random.randint(50, 100) #Generate number between (50-100)
+
+        if percentage >= 90:
+            letter_grade = grade_list[0] #A
+        elif percentage >= 80:
+            letter_grade = grade_list[1] #B
+        elif percentage >= 70:
+            letter_grade = grade_list[2] #C
+        elif percentage >= 60:
+            letter_grade = grade_list[3] #D
+        else:
+            letter_grade = grade_list[4] #F 
+
+        return letter_grade, f'{percentage}%' #Ex: [Erick, B]
+
+    def all_list(self):
+        names = self.student_names() #We get all the names contained in the list
+        full_report = [] #New empty list
+
+        for name in names: #This just means for every (item in this case name) in the list we
+            letter, percent = self.student_letter_grade() #Get [grade_list, pecentage] in a variable 
+            full_report.append([name, letter, percent]) # return to the empty list and everything into one single list called full_report
+            
+        return full_report
 
 if __name__ == "__main__":
     student = Student("name")
-    grades = Grade()
 
-    print(student.student_names())
+    print(student.all_list())
 
 """
 THis should be the layout more or less if possible 
@@ -39,4 +56,10 @@ Ex: ["Erick", "A", 100!]
 """
 step 1: 
 TO start lets just get a list of names, like consisting of 5 people!"
+step 2:
+Now get grade list!
+step 3; 
+use zip
+step 4:
+
 """
