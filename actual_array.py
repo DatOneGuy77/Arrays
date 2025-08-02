ee0cf27c1c614472c9039c1dd097746fe50bf1b7
Erick Gonzalez Cruz <176557431+DatOneGuy77@users.noreply.github.com>
2025-08-01 22:28:13 -0700

    new comments

--- actual_array.py
+++ actual_array.py
@@ -6,15 +6,17 @@ File: Working with an actual array
 
 import numpy as np 
 
+"""Using Addition in Arrays"""
 # my_array = np.array([50, 100, 200, 400, 800, 1600])
 # my_array_2 = np.array([30, 90, 140, 293, 324, 743])
 
 # addition = my_array + my_array_2
 
 # print(addition)
+# #Output: [ 80 190 340 693 1124 2343]
 
 #-----------------------------------------------------------------------------------
-
+"""Finding the minimum and maxium value in an array"""
 # array1 = np.array([1, 2, 3, 4, 5])
 
 # min = np.min(array1)
@@ -26,7 +28,7 @@ import numpy as np
 # #Output: 5
 
 #-----------------------------------------------------------------------------------
-
+"""Sorting the array in order so (1, 2, 3) to (3, 2, 1) and vice versa!"""
 # array1 = np.array([8, 3, 5, 1, 6, 2, 4, 7])
 
 # sorting = np.sort(array1) #1-8
@@ -39,7 +41,7 @@ import numpy as np
 # Output: [8 7 6 5 4 3 2 1]
 
 #-----------------------------------------------------------------------------------
-
+"""Finding the mean and median value in an array!"""
 # array1 = np.array([1, 2, 3, 4, 5])
 
 # # Mean value 
@@ -53,14 +55,25 @@ import numpy as np
 # #Output: 3.0
 
 #-----------------------------------------------------------------------------------
-
+"""Multiplying the matricies"""
 matrix1 = np.array([[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]])
 matrix2 = np.array([[7.7, 8.8], [9.9, 10.10], [11.11, 12.12]])
 
-# Multiply the matricies
 result = np.dot(matrix1, matrix2)
 print(result)
 
-# Output: 
-# [[ 66.913   71.896 ]
-# [161.656 174.262]]
+#Output: 
+#[[ 66.913   71.896 ]
+#[161.656 174.262]]
+
+#-----------------------------------------------------------------------------------
+"""Transpose the matrix"""
+# matrix = np.array ([[1, 2, 3], [4, 5, 6]])
+
+# transposed_matrix = np.transpose(matrix)
+# print(transposed_matrix)
+
+# #Output:
+# #[[1 4]
+# # [2 5]
+# # [3 6]]
